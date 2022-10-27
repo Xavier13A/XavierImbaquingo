fcdd8dacf9e86cddba8ee82545dbd472dba513d5
Xavier13 <xav.imbaquingo@Bgmail.com>
2022-10-26 23:41:05 -0500

    Agregar archivos de proyecto.

---
+++ XavierImbaquingo/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XavierImbaquingo
+{
+
+    public partial class MainPage : ContentPage
+
+
+
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            string usuario = "Xavier2022";
+            string Contrasena = "uisrael2022";
+
+            string TUsuario = txtUsuario.Text;
+            string TContrasena = txtContraseña.Text;
+
+            if (usuario == TUsuario & Contrasena == TContrasena)
+            {
+                await Navigation.PushAsync(new Registro(usuario));
+                await DisplayAlert("Ingreso Exitoso !", "Datos Válidos para !!" + usuario, "Cerrar");
+
+
+
+
+            }
+            else
+            {
+                await DisplayAlert("Error !", "Usuario o Contraseña Incorrecta", "Cerrar");
+            }
+
+        }
+    }
+}
+        
+        
+
+        
+    
+
---
+++ XavierImbaquingo/Registro.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XavierImbaquingo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            //string nombre = txtNombre.Text;
+            double MontoInicial = Convert.ToDouble (txtMontoInicial.Text);
+            double PagoMensual = 3000- MontoInicial;
+            
+            double  Calular = (PagoMensual/5)*1.05;
+
+            txtCuotaMensual.Text = Calular.ToString();
+
+            DisplayAlert("Mesaje de alerta", Calular.ToString(), "Guardar");
+
+
+
+
+        }
+    }
+}
